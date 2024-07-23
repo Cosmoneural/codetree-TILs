3fcdd53de096ee5b9cd6c4ac581ace88c745407b
Cosmoneural <stellarspecial@gmail.com>
2024-07-23 09:48:49 +0000

    Title: 길이 단위 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 9.2, b = 1.3;
+    printf("%.1fft = %.1lfcm\n%.1lfmi = %.1lfcm", a, a*30.48, b , b*160934);
+    return 0;
+}

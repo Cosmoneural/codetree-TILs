8f156af57ef40a4eef6f10bfbce0519f9bf22308
Cosmoneural <stellarspecial@gmail.com>
2024-07-24 10:58:44 +0000

    Title: 합을 복사하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/합을 복사하기/copy-the-sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1,b=2,c=3;
+    //아 공부하기 싫다.
+    printf("%d %d %d", (a+b+c),(a+b+c),(a+b+c));
+    //그래도 '해야'한다
+    //해야해야해야~
+    return 0;
+}

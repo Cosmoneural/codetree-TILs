7e8b1833e46f5dbc352a6ee135a7aebc523ee08f
Cosmoneural <stellarspecial@gmail.com>
2024-07-29 09:16:13 +0000

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/간단한 사칙연산/simple-arithmetic-operation.c
+++ 240729/간단한 사칙연산/simple-arithmetic-operation.c
@@ -2,7 +2,9 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.^=^
-    int a = 2, b = 5;
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
     printf("%d\n%d\n%d\n%d\n",a+b,a-b,a/b,a%b);
     return 0;
 }

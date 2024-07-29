7b7ae34eff3855f11a0932d0aed29e25b74ef7b8
Cosmoneural <stellarspecial@gmail.com>
2024-07-29 09:01:28 +0000

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/전화번호 바꾸기/change-phone-number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주란말이야!!!!!!11!!!11!111!!1!11111111!
+    int b,c;
+    scanf("010-%d-%d", &b, &c);
+    printf("010-%d-%d",c, b);
+    return 0;
+}

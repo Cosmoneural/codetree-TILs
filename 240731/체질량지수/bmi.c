699a2091ff17f0af7d1d604a0d74ba3c354f4270
Cosmoneural <stellarspecial@gmail.com>
2024-07-31 12:04:00 +0000

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240731/체질량지수/bmi.c
+++ 240731/체질량지수/bmi.c
@@ -7,6 +7,8 @@ int main() {
     /*
     printf("a=%d b=%d\n", a , b);
     *///
+    
+
     int a1 = 10000*b;
     int b1 = a*a; //a^2 xxxx a*a a*a*a
 
@@ -16,9 +18,7 @@ int main() {
     
     printf("%d", c);
 
-    
+    if(c>=25);
     printf("\nObesity");
-    
-    
     return 0;
 }

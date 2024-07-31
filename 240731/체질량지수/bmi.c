638bdaa14ceeba2b56441864fd89d52b23379b68
Cosmoneural <stellarspecial@gmail.com>
2024-07-31 12:01:04 +0000

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240731/체질량지수/bmi.c
+++ 240731/체질량지수/bmi.c
@@ -4,16 +4,21 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-
+    /*
     printf("a=%d b=%d\n", a , b);
-    
+    *///
     int a1 = 10000*b;
-    int b1 = a*a;
+    int b1 = a*a; //a^2 xxxx a*a a*a*a
 
-    printf("%d : %d", a1, b1);
+    //printf("%d : %d\n", a1, b1);
 
-    double c = (double)(10000*b/(a^2));
-    printf("%.2f", c);
+    int c = (a1/b1);
+    
+    printf("%d", c);
+
+    
     printf("\nObesity");
+    
+    
     return 0;
 }

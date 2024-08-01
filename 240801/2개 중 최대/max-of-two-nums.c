abbea2866fcb3df215718ed16543da75b51187b5
Cosmoneural <stellarspecial@gmail.com>
2024-08-01 10:08:08 +0000

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/2개 중 최대/max-of-two-nums.c
+++ 240801/2개 중 최대/max-of-two-nums.c
@@ -4,8 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    if(a<b){
+    if(a<=b){
         printf("%d",b);
+    }else{
+        printf("%d",a);
     }
     return 0;
 }

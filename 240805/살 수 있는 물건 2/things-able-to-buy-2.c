38e6d2f057da793d93b175a401e52fae7e0efa21
Cosmoneural <stellarspecial@gmail.com>
2024-08-05 05:51:58 +0000

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    if(a>=3000){
+    printf("book");
+    }else if(1000>=a){
+    printf("mask");
+    }else{
+    printf("no");
+    }
+    return 0;
+}

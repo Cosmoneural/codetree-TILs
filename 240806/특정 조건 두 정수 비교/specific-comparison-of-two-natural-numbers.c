6509d55dc53c748a38e4b07635c3b19a68df6ad3
Cosmoneural <stellarspecial@gmail.com>
2024-08-06 05:22:44 +0000

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a<b){
+        printf("%d",1);
+    }else{
+        printf("%d",0);
+    }if (a==b){
+        printf(" %d",1);
+    }else {
+        printf(" %d",0);
+    }
+    return 0;
+}

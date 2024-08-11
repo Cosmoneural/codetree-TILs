261bfaa11783ceb0c43787582f6a1ef34ce5ae62
Cosmoneural <stellarspecial@gmail.com>
2024-08-11 03:58:22 +0000

    Title: 세 정수의 최솟값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/세 정수의 최솟값/minimum-of-three-numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,min;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b && a<=c) {
+        //printf("1\n");
+        if(b>=c) {
+            min = c;
+        } else {
+            min = b;
+        }
+    } else {
+        min = a;
+    }
+    printf("%d", min);
+    return 0;
+}

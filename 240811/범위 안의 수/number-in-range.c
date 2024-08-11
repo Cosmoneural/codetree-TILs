0b60e6d998f4882330894bc9fe824663afbfebff
Cosmoneural <stellarspecial@gmail.com>
2024-08-11 02:49:46 +0000

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/범위 안의 수/number-in-range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요를레이히.
+    int a;
+    scanf("%d",&a);
+    if(a>=10 && 20>=a){
+        printf("yes");
+    }else{
+        printf("no");
+    }
+    return 0;
+}

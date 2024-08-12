405cf14fd2c66d5f42765fde27563b1b37cb15af
Cosmoneural <stellarspecial@gmail.com>
2024-08-12 11:58:47 +0000

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240812/장학금/scholarship.c
+++ 240812/장학금/scholarship.c
@@ -5,9 +5,12 @@ int main() {
     int a,b;
     scanf("%d %d",&a,&b);
     if(a>=90){
+        
         if(b>=95){
             printf("100000");
-        }else if(b>=90){
+        }
+        
+        if(b>=90){
             printf("50000");
         }
     }else{

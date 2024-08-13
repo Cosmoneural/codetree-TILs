83c56e3cff75d01bbf66a7729b714bef5d80020c
Cosmoneural <stellarspecial@gmail.com>
2024-08-13 06:11:09 +0000

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
+++ 240813/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a1,a2,b1,b2;
     scanf("%d %d %d %d",&a1,&a2,&b1,&b2);
-    if(a1>b1){
+    if(a1>b1|| (a1==b1 && a2>b2)){
         printf("A");
-    }else if (b1>a1){
+    }else if (b1>a1 || (a1==b1 && b2>a2)){
         printf("B");
     }
     return 0;

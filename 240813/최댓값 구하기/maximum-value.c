a48939f6e5fc914b5acdc2dace50ab1d4b14b40e
Cosmoneural <stellarspecial@gmail.com>
2024-08-13 09:50:08 +0000

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/최댓값 구하기/maximum-value.c
+++ 240813/최댓값 구하기/maximum-value.c
@@ -4,11 +4,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
-    if(b=<a && a>=c){
+    if(b<=a && a>=c){
         printf("%d",a);
-    }else if(a=<b && b>=c){
+    }else if(a<=b && b>=c){
         printf("%d",b);
-    }else if(b=<c && c>=a){
+    }else if(b<=c && c>=a){
         printf("%d",c);
     }
     return 0;

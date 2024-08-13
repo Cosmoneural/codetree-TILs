a3c2b5f08c9746b3ba95522cd14b9152aab50569
Cosmoneural <stellarspecial@gmail.com>
2024-08-13 06:26:16 +0000

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/두 사람/two-person.c
+++ 240813/두 사람/two-person.c
@@ -4,8 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a1,b1;
     char a2,b2;
-    scanf("%d %d",&a1,&b1);
-    scanf("%s %s",&a2,&b2);
+    scanf("%d %c %d %c",&a1,&a2,&b1,&b2);
     if((a1>=19 && a2=='M') || (b1>=19 && b2=='M')){
         printf("1");
     }else{

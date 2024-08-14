eba6b604a22e7db5b787873ccfadf59060693424
Cosmoneural <stellarspecial@gmail.com>
2024-08-14 08:12:39 +0000

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/홀수만 출력/output-only-odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    for(int i=a,n=b;i<b+2;i+=2){
+        printf("%d ",i);
+    }
+    return 0;
+}

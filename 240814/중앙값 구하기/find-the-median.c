fcb6ba013a2517adc1c5d25840c452f2ae4823ad
Cosmoneural <stellarspecial@gmail.com>
2024-08-14 10:41:08 +0000

    Title: 중앙값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240814/중앙값 구하기/find-the-median.c
+++ 240814/중앙값 구하기/find-the-median.c
@@ -3,12 +3,12 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
+    scanf("%d",&a,&b,&c);
     if((b>a && a>c) || (c>a && a>b)){
         printf("%d",a);
     }if((a>b && b>c) || (c>b && b>a)){
         printf("%d",b);
-    }if((b>c && c>a)){
+    }if((a>c && c>b) || (b>c && c>a)){
         printf("%d",c);
     }
     return 0;

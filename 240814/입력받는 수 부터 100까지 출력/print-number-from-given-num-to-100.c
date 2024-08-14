118db929f0ec8e338bff5db6afbc0c6e790b8f91
Cosmoneural <stellarspecial@gmail.com>
2024-08-14 07:48:32 +0000

    Title: 입력받는 수 부터 100까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240814/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.c
+++ 240814/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.c
@@ -2,8 +2,10 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    for(int n=98;n<101;n++){
-        printf("%d ",n);
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i<101;i++){
+        printf("%d ",i);
     }
     return 0;
 }

669b2476474a9e3329c4fd277530a8370c311ea9
Cosmoneural <stellarspecial@gmail.com>
2024-08-16 11:28:50 +0000

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int b=1;
+    scanf("%d",&a);
+    while(b<=a){
+        printf("*\n");
+        b++;
+    }
+    return 0;
+}

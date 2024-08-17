469952a9732621aac601d19055fd9c0d8c6fb65e
Cosmoneural <stellarspecial@gmail.com>
2024-08-17 14:27:39 +0000

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240817/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char g;
+    scanf("%c",&g);
+    int a=8;
+    for(int i=1;i<=a;i++){
+        printf("%c",g);
+    }
+    return 0;
+}

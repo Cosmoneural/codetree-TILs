4f5dfc5a2798a20ee96667951c6099dedb4af4da
Cosmoneural <stellarspecial@gmail.com>
2024-08-21 13:31:18 +0000

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240821/a/b 출력/a-divide-b.c
+++ 240821/a/b 출력/a-divide-b.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    printf("%.20Lf", (long double)a/b);
+    printf("%.19Lf", (long double)a/b);
     return 0;
 }

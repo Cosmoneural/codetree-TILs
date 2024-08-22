4fd5319c353b65016625ea1e942af9e28ad0f600
Cosmoneural <stellarspecial@gmail.com>
2024-08-22 12:40:44 +0000

    Title: 높은 수에서 낮은 수까지, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/높은 수에서 낮은 수까지/from-high-to-low.c
+++ 240822/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -4,11 +4,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    if(a>b){
+    if(a>=b){
         for(int i=a;i>=b;i--){
             printf("%d ",i);
         }
-    }if(b>a){
+    }else if(b>=a){
         for(int i=b;i>=a;i--){
             printf("%d ",i);
         }

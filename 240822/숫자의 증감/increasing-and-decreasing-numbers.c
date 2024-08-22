4287faed14a1d8172140d09028fbe682abf75ae4
Cosmoneural <stellarspecial@gmail.com>
2024-08-22 12:10:10 +0000

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%c %d",&a,&b);
+    if(a=='A'){
+        for(int i=1;i<=b;i++){
+            printf("%d ",i);
+        }
+    }if(a=='D'){
+        for(int b;b>=1;b--){
+            printf("%d ",b);
+        }
+    }
+    return 0;
+}

b44997e0bbd01f64836f5b4808fd506e896265b9
Cosmoneural <stellarspecial@gmail.com>
2024-08-23 13:42:12 +0000

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240823/a/b 출력/a-divide-b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요를레이히
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d.",a/b);
+
+    int r = a%b;
+    for(int i=0;i<21;i++){
+        r = r*10; //r+=10;
+        printf("%d", r/b);
+        r = r%b;
+    }
+    return 0;
+}

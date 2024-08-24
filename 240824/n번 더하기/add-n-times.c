12fbca50b70d5dc353ac4769f677d9e3e4899206
Cosmoneural <stellarspecial@gmail.com>
2024-08-24 02:52:03 +0000

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240824/n번 더하기/add-n-times.c
+++ 240824/n번 더하기/add-n-times.c
@@ -2,11 +2,16 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b;
-    scanf("%d %d",&a,&b);
-    int n=a+b;
-    for(int i=n;i<=41;i+=b){
-        printf("%d\n",i);
+    int a,n;
+    scanf("%d %d",&a,&n);
+    int ab=a+n; // 일단 한번 더했어.
+
+    for(int i=1;i<=n;i++) {
+    //for(int i=0;i<n;i++){ //n번 반복하려고 합니다.
+        //1 2 3 4 5 6 => i
+        //0 1 2 3 4 5 => i
+        printf("%d\n", ab); // 일단 그리고 출력
+        ab = ab + n; // a에 n을 더하는 과정
     }
     return 0;
 }

2610f923de906b8e6329b32b506402c6fffeb299
Cosmoneural <stellarspecial@gmail.com>
2024-08-25 10:20:58 +0000

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240825/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d ",a);
+    for(int i=a;i<=b-1;){
+        if(i%2==1){
+            i*=2;
+            printf("%d ",i);
+        }else{
+            i+=3;
+            printf("%d ",i);
+        }if(i>=b){
+        }
+    }
+    return 0;
+}

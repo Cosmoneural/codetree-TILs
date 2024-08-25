2a5d1e87258b092084b1985cb74eb8530a2df161
Cosmoneural <stellarspecial@gmail.com>
2024-08-25 10:37:02 +0000

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240825/a 부터 b 까지/a-to-b.c
+++ 240825/a 부터 b 까지/a-to-b.c
@@ -5,16 +5,16 @@ int main() {
     int a,b;
     scanf("%d %d",&a,&b);
     printf("%d ",a);
-    for(int i=a;i<=b-1;){
+    for(int i=a;i<b;){
         if(i%2==1){
             i*=2;
             printf("%d ",i);
         }else{
             i+=3;
             printf("%d ",i);
+        }if(i>=b){
+            printf("");
         }
-        
-
     }
     return 0;
 }

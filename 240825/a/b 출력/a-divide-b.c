b7eab2bad5e85cace1c9b6dea627ec4f0328eff3
Cosmoneural <stellarspecial@gmail.com>
2024-08-25 13:14:38 +0000

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240825/a/b 출력/a-divide-b.c
+++ 240825/a/b 출력/a-divide-b.c
@@ -5,15 +5,13 @@ int main() {
     int a,b;
     scanf("%d %d",&a,&b);
     printf("%d.",a/b);
-    int c;
-    c=a%b;
-    for(int i=0;i<20;i++){
+    int c=a % b;
+    int i=0;
+    while(i<20){
         c*=10;
         printf("%d",c/b);
-        
         c%=b;
+        i++;
     }
     return 0;
 }
-
-//for 로 1번, while로 1번하기

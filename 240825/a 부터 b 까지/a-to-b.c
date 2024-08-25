e07a492d9b0024ae2374a0eb45aff1990a661807
Cosmoneural <stellarspecial@gmail.com>
2024-08-25 11:09:38 +0000

    Title: a 부터 b 까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240825/a 부터 b 까지/a-to-b.c
+++ 240825/a 부터 b 까지/a-to-b.c
@@ -4,20 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    printf("%d ",a);
-    for(int i=a;i<=b;){
-
-        
+    int i=a;
+    while(i<=b){
+        printf("%d ",i);
         if(i%2==1){
             i*=2;
-            if(i<=b){
-            printf("%d ",i);
-            }
         }else{
             i+=3;
-            if(i<=b){
-            printf("%d ",i);}
         }
+   
     }
     return 0;
 }

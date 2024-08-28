26acde5edc5fc361ec7a42e21bbe5561d10c953e
Cosmoneural <stellarspecial@gmail.com>
2024-08-28 12:33:32 +0000

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240828/청소당번 정하기/cleaning-numbering.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    int cnt=0,cnt2=0,cnt3=0;
+    int i=1;
+    while(i<=n){
+        if(i%12==0){
+            cnt3++;
+        }if(i%3==0){
+            cnt2++;
+        
+        }if(i%2==0 && i%3!=0){
+            cnt++;
+        }
+        i++;
+    }printf("%d %d %d",cnt,cnt2,cnt3);
+    return 0;
+}

aff96a8fb314b7fe20b44d2551fa5e865f613f85
Cosmoneural <stellarspecial@gmail.com>
2024-08-29 11:35:31 +0000

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240829/369 게임/369-game.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        int digit=i%10;
+        if((digit==3 || digit==6 || digit==9)|| digit%3==0){
+            printf("0 ");
+        }else{
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}

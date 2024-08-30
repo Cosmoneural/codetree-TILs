2d73e30d4321b6e8a2633e34bc0c9dfd53920d14
Cosmoneural <stellarspecial@gmail.com>
2024-08-30 07:08:53 +0000

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240830/369 게임/369-game.c
+++ 240830/369 게임/369-game.c
@@ -2,19 +2,21 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-        int a;
-    scanf("%d", &a);
-    for(int i=1;i<=a;i++){
-        int temp = i;
-        int r = temp%10;
-        while (temp>0){
-        
-        if(i%3==0 || r == 3 || r == 6 || r == 9) {
-            printf("0 ");
-        }temp = temp / 10;
-
-        }
-        printf("%d ", i);
-        }
-      return 0;  
-    }
+	int n;
+	scanf("%d",&n);
+	int i=1;
+	while(i<=n){
+		int r= i%10;
+		if(i%3==0){
+			printf("0 ");
+		}
+		else if(r==3 || r==6 || r==9){
+			printf("0 ");
+		}else if(i/10==3||i/10==6||i/10==9){
+			printf("0 ");
+		}else{
+			printf("%d ",i);
+		}i++;
+	}
+    return 0;
+}

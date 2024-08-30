35a0efbe7c5d86bc4bb6a7c5dda0e516c370a429
Cosmoneural <stellarspecial@gmail.com>
2024-08-30 09:34:24 +0000

    Title: 홀수만 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240830/홀수만 출력하기/print-only-odd-numbers.c
+++ 240830/홀수만 출력하기/print-only-odd-numbers.c
@@ -2,12 +2,16 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
-    int i=1;
-    while(scanf("%d",&n)==1){
-        if(n%3==0 && n%2==1){
-            printf("%d\n",n);
+    int a, n;
+    scanf("%d", & a);
+
+    int i = 0;
+    while (i < a) {
+        scanf("%d", & n);
+        if (n % 3 == 0 && n % 2 == 1) {
+            printf("%d\n", n);
         }
+        i++;
     }
-    return 0;
+
 }

dc897bcd03532f2cd117d30bec59fb0fe176c348
Cosmoneural <stellarspecial@gmail.com>
2024-09-01 08:05:59 +0000

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/홀수만 더하기/add-only-odd-numbers.c
+++ 240901/홀수만 더하기/add-only-odd-numbers.c
@@ -3,13 +3,13 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,n;
-    scanf("%d",&a);
+    scanf("%d",&a); // 입력되어질 정수의 갯수 4
     int i=1;
     int sum=0;
     while(i<=a){
         scanf("%d",&n);
         if(n%2==1 && n%3==0){
-            sum=sum+1;
+            sum+=n;
         }i++;
     }printf("%d",sum);
     return 0;

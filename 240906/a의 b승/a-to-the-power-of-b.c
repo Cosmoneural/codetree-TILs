103e971065d56382c6744a3dee70b8c1a2f6e409
Cosmoneural <stellarspecial@gmail.com>
2024-09-06 11:53:40 +0000

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int power = a;
+    //a 자체를 b번 곱한다.
+    for(int i=1;i<b;i++){
+        power = power * a;
+    }
+    printf("%d", power);
+/*
+    int s=1;
+    int i=a;
+    while(i<=b){
+        s*=a;
+        i++;
+    }printf("%d",s);
+    return 0;
+*/
+}

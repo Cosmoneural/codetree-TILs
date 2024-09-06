4fde3cb35e9a7fa138b81a65100895c8962bfb43
Cosmoneural <stellarspecial@gmail.com>
2024-09-06 11:58:39 +0000

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/a의 b승/a-to-the-power-of-b.c
+++ 240906/a의 b승/a-to-the-power-of-b.c
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d %d",&a,&b);
-    int power = a;
+    int power = 1;
     //a 자체를 b번 곱한다.
-    for(int i=1;i<b;i++){
+    for(int i=1;i<=b;i++){
         power = power * a;
     }
     printf("%d", power);

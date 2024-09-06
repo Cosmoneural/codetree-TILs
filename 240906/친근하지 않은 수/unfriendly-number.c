6446fc7d75c08021f30ffd6aa80c9f7756737145
Cosmoneural <stellarspecial@gmail.com>
2024-09-06 12:34:11 +0000

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/친근하지 않은 수/unfriendly-number.c
+++ 240906/친근하지 않은 수/unfriendly-number.c
@@ -6,9 +6,9 @@ int main() {
     scanf("%d",&n);
     int cnt=0;
     for(int i=1;i<=n;i++){
-        if(i%2!=0 && i%3!=0 && i%5!=0){
-            cnt+=1;
-        }
+        if(i%2==0 || i%3==0 || i%5==0){
+            continue;
+        }cnt++;
     }printf("%d",cnt);
     return 0;
 }

a0dd83579e6cf91b6727f82fece987c24c81c4f2
Cosmoneural <stellarspecial@gmail.com>
2024-09-11 13:11:49 +0000

    Title: 사각형의 넓이 계속 계산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char c;
+    while(1){
+        scanf("%d %d %c",&a,&b,&c);
+        printf("%d\n",a*b);
+        if(c=='C'){
+            break;
+        }
+    }
+    return 0;
+}

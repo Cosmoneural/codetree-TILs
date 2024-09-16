59f5c3c0c83ff23569947311cbe50bd47554e590
Cosmoneural <stellarspecial@gmail.com>
2024-09-16 11:43:23 +0000

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/조건에 따른 연산/operatino-by-rule.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt=0;
+    int n;
+    scanf("%d",&n);
+    while(1){
+        if(n>=1000){
+            break;
+        }if(n%2==0){
+            n*=3;
+            n+=1;
+            cnt++;
+        }if(n%2==1){
+            n*=2;
+            n+=2;
+            cnt++;
+        }
+    }printf("%d",cnt);
+    return 0;
+}

aec0fe380a6c50de5bf30232f0d947fd950a3ebc
Cosmoneural <stellarspecial@gmail.com>
2024-09-19 01:08:45 +0000

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240919/소수 판별기/prime-number-discriminator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    bool ljkoa=true;
+    for(int i=1;i<=a;i++){
+        if(a%i==0 && a%1==0){
+            ljkoa=false;
+        }
+    }if(ljkoa==true){
+        printf("C");
+    }else{
+        printf("P");
+    }
+    return 0;
+}

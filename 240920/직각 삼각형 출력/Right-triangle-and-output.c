e90af96cd8c9d0db7646160a9c839ecbd9957d3c
Cosmoneural <stellarspecial@gmail.com>
2024-09-20 01:13:40 +0000

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/직각 삼각형 출력/Right-triangle-and-output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n*2;i+=2){
+         for(int j=1;j<=i;j++){
+            printf("*");
+        }printf("\n");
+    }
+    return 0;
+}

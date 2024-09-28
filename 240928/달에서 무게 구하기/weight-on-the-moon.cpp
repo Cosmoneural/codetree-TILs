73fa65cc4aa52feedc502dde2e876c2d63c03f4b
Cosmoneural <stellarspecial@gmail.com>
2024-09-28 13:20:07 +0000

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240928/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=13;
+    cout<<fixed;
+    double b=0.165000;
+    cout.precision(6);
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    return 0;
+}

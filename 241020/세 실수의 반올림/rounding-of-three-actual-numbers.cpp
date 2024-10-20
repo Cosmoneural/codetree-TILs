badc30cf35aed4c8e0375846990d10fd047702a7
Cosmoneural <stellarspecial@gmail.com>
2024-10-20 09:54:04 +0000

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double ä,b,ç;
+    cin>>ä>>b>>ç;
+    cout<<fixed;
+    cout.precision(3);
+    cout<<ä<<endl<<b<<endl<<ç;
+    return 0;
+}

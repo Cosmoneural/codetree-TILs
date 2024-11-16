0d4c93e96769da7e97930da9a2d51dc8d2f5e217
Cosmoneural <stellarspecial@gmail.com>
2024-11-16 12:17:17 +0000

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241116/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int coconut,banana,horse;
+    cin>>coconut>>banana>>horse;
+    cout<<coconut+banana+horse<<endl<<(coconut+banana+horse)/3;
+    return 0;
+}

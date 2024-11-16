2294743af2d287821e827df613b2876ae94ac3e9
Cosmoneural <stellarspecial@gmail.com>
2024-11-16 12:22:15 +0000

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241116/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int what,how,who;
+    cin>>what>>how>>who;
+    cout<<what+how+who<<endl<<(what+how+who)/3<<endl<<(what+how+who)-(what+how+who)/3;
+    return 0;
+}

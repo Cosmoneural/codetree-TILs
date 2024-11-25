5d10330df4405d9f671c4e9d831b988bc378f327
Cosmoneural <stellarspecial@gmail.com>
2024-11-25 10:56:17 +0000

    Title: 2개 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241125/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sysprsgjoihgrenjksgekjbsrghuio,b;
+    cin>>sysprsgjoihgrenjksgekjbsrghuio>>b;
+    if(sysprsgjoihgrenjksgekjbsrghuio>b){
+        cout<<sysprsgjoihgrenjksgekjbsrghuio;
+    }else{
+        cout<<b;
+    }
+    return 0;
+}

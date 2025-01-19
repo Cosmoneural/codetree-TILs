bf579c354680525d7e19d6716a0ab76aceb7f173
Cosmoneural <stellarspecial@gmail.com>
2025-01-19 05:40:01 +0000

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b>c && a==c){
+        cout<<"1";
+    }else if(c>b && a==b){
+        cout<<"1";
+    }else{
+        cout<<"0";
+    }
+    if(a==b && c==b){
+        cout<<" 1";
+    }else{
+        cout<<" 0";
+    }
+    return 0;
+}

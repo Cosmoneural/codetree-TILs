c03bb3b71fee674481c791338dc81a9ac8b6d1a4
Cosmoneural <stellarspecial@gmail.com>
2025-01-20 03:06:47 +0000

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/장학금/scholarship.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 에햇해새해새해해새해ㅐ햇햏새해해햏해해해해햏ㅎ
+    int a,b;
+    cin>>a>>b;
+    if(a>=90){
+        if(b>=95){
+            cout<<"100000";
+        }else if(b>=90){
+            cout<<"50000";
+        }
+            }else{
+            cout<<"0";
+        }
+    return 0;
+}

a3ca42343c49c2907defcad051ca03ac0c4867b7
Cosmoneural <stellarspecial@gmail.com>
2025-01-20 08:26:42 +0000

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250120/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 250120/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int a,b,c,d;
     cin>>a>>b>>c>>d;
-    if(a>b || (a==b && c>d)){
+    if(a>c || (a==c && b>d)){
         cout<<"A";
     }else{
         cout<<"B";

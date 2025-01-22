654b4f67b23ee7c96e25bea4beee16fe877e17d6
Cosmoneural <stellarspecial@gmail.com>
2025-01-22 04:54:52 +0000

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250122/윤년인가/is-leap-year.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if (a % 4==0) {
+        if (a % 100 == 0) {
+            if (a % 400 == 0) {
+                cout << "true";
+            } else {
+                cout << "false";
+            }
+        } else {
+            cout << "true";
+        }
+        //cout << "true";
+    } 
+    else {
+        cout << "false";
+    }
+    return 0;
+}

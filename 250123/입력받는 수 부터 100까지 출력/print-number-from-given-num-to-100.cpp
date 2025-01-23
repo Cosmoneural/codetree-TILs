4141a7c51f22adf002769ab5af147ec2de45965d
Cosmoneural <stellarspecial@gmail.com>
2025-01-23 08:49:41 +0000

    Title: 입력받는 수 부터 100까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250123/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
+++ 250123/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     // Please write your code here.
     int a;
-    for(int i=a;i>=100;i++){
+    cin>>a;
+    for(int i=a;i<=100;i++){
         cout<<i<<" ";
     }
     return 0;

d869306d31c4410dbcb8431e0c831bea288ec436
Cosmoneural <stellarspecial@gmail.com>
2025-01-25 13:37:17 +0000

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250125/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    for(int i=a;i<=5*a;i+=a){
+        cout<<i<<" ";
+    }
+    return 0;
+}

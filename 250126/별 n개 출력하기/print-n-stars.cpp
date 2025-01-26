d4349a5e2e428d1e035eca4dbf060a89ca75ae87
Cosmoneural <stellarspecial@gmail.com>
2025-01-26 05:01:35 +0000

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250126/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    int i=1;
+    while(i<=a){
+        cout<<"*"<<endl;
+        i++;
+    }
+    return 0;
+}

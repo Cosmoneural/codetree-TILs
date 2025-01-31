411494c70faf50053d118f9320e9a7e2308ec485
Cosmoneural <stellarspecial@gmail.com>
2025-01-31 12:54:04 +0000

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250131/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt=0,cot=0;
+    for(int i=1;i<=10;i++){
+        int a;
+        cin>>a;
+        if(a%3==0){
+            cnt++;
+        }  else{
+        } if(a%5==0){
+            cot++;
+        }else{
+            
+        }
+    }cout<<cnt<<" "<<cot;
+    return 0;
+}

68db3ed43c94ca5a9325eb67d655bb83129e4c9f
Cosmoneural <stellarspecial@gmail.com>
2025-01-31 13:43:25 +0000

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250131/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    int sum=0;
+    for(int i=1;i<=a;i++){
+        int n;
+        cin>>n;
+        if(n%2==1 && n%3==0){
+            sum+=a;
+        }
+    }cout<<sum;
+    return 0;
+}

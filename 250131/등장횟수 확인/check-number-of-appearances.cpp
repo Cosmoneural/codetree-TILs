1499ca7c5e0ac8746c103f6dfa62a866c71d5393
Cosmoneural <stellarspecial@gmail.com>
2025-01-31 13:27:11 +0000

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250131/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt=0;
+    for(int i=1;i<=5;i++){
+        int a;
+        cin>>a;
+        if(a%2==0){
+            cnt++;
+        }
+    }cout<<cnt;
+    return 0;
+}

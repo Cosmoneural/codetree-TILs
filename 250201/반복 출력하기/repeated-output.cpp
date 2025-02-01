412936583e3120b395d9441c53d4dbf83964af94
Cosmoneural <stellarspecial@gmail.com>
2025-02-01 08:09:34 +0000

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+void wow(int n){
+    for(int i=1;i<=n;i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+
+
+int main() {
+    int a;
+    cin>>a;
+    wow(a);
+    // Write your code here!
+
+    return 0;
+}

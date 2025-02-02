738d8cd449f16b445aade29f7d9e753024743fee
Cosmoneural <stellarspecial@gmail.com>
2025-02-02 03:02:46 +0000

    Title: 1부터의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    int sum=0;
+    for(int i=1;i<=100;i++){
+        sum+=i;
+        if(sum>=a){
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}

8439a8c98587b021c153dac59b33b0941a49ab5d
Cosmoneural <stellarspecial@gmail.com>
2025-02-02 03:10:04 +0000

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(true){
+        int i;
+        cin>>i;
+        if(i==0){
+            break; 
+        }cout<<i<<endl;
+    }
+    return 0;
+}

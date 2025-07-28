ece3f426e2a200c4739ae9dcdc1531fc4f83591a
Cosmoneural <stellarspecial@gmail.com>
2025-07-28 13:16:22 +0000

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250728/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void vlock(int n){
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_";
+        cout<<endl;
+    }
+}
+
+int main() {
+    int a;
+    cin>>a;
+    vlock(a);
+    return 0;
+}

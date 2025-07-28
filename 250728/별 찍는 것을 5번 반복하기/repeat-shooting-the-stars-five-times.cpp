84ac078c67286d73a8582dbde5c3ed4061483290
Cosmoneural <stellarspecial@gmail.com>
2025-07-28 13:05:07 +0000

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250728/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    void yeah() {
+        for(int i=0;i<10;i++){
+            cout<<"*";
+        }
+    }
+
+    for(int j=0;j<4;j++){
+        yeah;
+    }
+    return 0;
+}
